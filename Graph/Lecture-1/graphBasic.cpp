5cc11dbb2548e2feef3679378d2cbd5f2f6e87f5
Arpit Kumar <arpitkumar19842@gmail.com>
2025-10-14 08:56:00 +0530

    Started Learning Graph

---
+++ Graph/Lecture-1/graphBasic.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*
+        1. Multigraph : Undirected, multiple edges possible between 2 vertices and edge to itself also possible 
+
+        2. Simple Graph : Undirected, multiple edges and loops are not allowed
+
+        3. Complete Graph : each node is connected to all other nodes directly
+
+        4. Connected Graph : We have direct or indirect path for any possible pair of vertices
+
+        Path : Path is also a graph whose vertices can be arranged in a sequence
+        Cycle : Cycle is also a graph whose vertices can be arranged in a cyclic 
+        sequence
+
+
+        5. DAG (Directed Acyclic Graph) : Directed graph without cycle
+        6. Degree : Degree of a vertex is a total no of edges incident to it or away from it 
+
+        Indegree : No. of Incoming edges
+        Outdegree : No. of Outgoing edges
+
+
+        7. Tree is also a graph which never contain cycle
+            No of edge = No of node - 1
+
+            Tree is a connected graph
+            Min no of edge in connected graph : No of node - 1
+
+        8. Undirected Complete graph edges = vC2 = v! / (2! * (v-2)!)
+
+        9. Directed Cmplete Graph edges = v * |v-1|                     (max)
+    */
+}
